d88696acc88f3c1fff9b01d2992561faacd63168
saikumarbonigala <saikumarbonigala@users.noreply.github.com>
2016-09-19 20:38:34 +0530

    Q1,day4

--- Solution.java
+++ Solution.java
@@ -1,24 +1,31 @@
 import java.util.*;
 
-class Embed
+class AddDigits
 {
-	public String embedWord(String embed,String str)
+	public int sumDigits(String str)
 	{
-		int len=embed.length();
-		String sb=embed.substring(0,len/2)+str+embed.substring(len/2);
-		return sb;
+		int sum=0;
+		char c;
+		for(int i=0;i<str.length();i++)
+		{
+			c=str.charAt(i);
+			if(Character.isDigit(c))
+				sum=sum+Integer.parseInt(String.valueOf(c));
+		}
+		return sum;
 	}
 }
 
+
 class Solution
 {
-	public static void main(String args[])
+	public static void main(String[] args) 
 	{
-		Embed obj = new Embed();
-
-		System.out.println(obj.embedWord("<<>>", "Hello"));
-		System.out.println(obj.embedWord("<>", "HTML"));
-		System.out.println(obj.embedWord("[]", "1,2,3,4,5"));
-		System.out.println(obj.embedWord("{[]}", "a:1, b:2, c:3"));
+		Scanner sc = new Scanner(System.in);
+		
+		AddDigits obj = new AddDigits();
+		System.out.println(obj.sumDigits("aa1bc2d3"));
+		System.out.println(obj.sumDigits("aa11b33"));
+		System.out.println(obj.sumDigits("nodigits"));
 	}
 }
